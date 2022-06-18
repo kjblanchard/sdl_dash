d34db3270cde55d3957b9d99c0b1d0b5f20cb57c
Kevin Blanchard <blanchardkevinj@gmail.com>
2022-06-18 16:47:23 -0400

    Get it compiling on linux

--- supergoon_engine/supergoon_engine/sound/sound.cpp
+++ supergoon_engine/supergoon_engine/sound/sound.cpp
@@ -31,9 +31,9 @@ Sound::Setup()
     coreSystem->setSoftwareFormat(0, FMOD_SPEAKERMODE_STEREO, 0);
     system->initialize(1024, FMOD_INIT_NORMAL, FMOD_INIT_NORMAL, nullptr);
     FMOD::Studio::Bank *mainBank = nullptr;
-    result = system->loadBankFile("assets/sfx/desktop/Master.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &mainBank);
+    result = system->loadBankFile("assets/sfx/Desktop/Master.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &mainBank);
     FMOD::Studio::Bank *stringsBank = nullptr;
-    result = system->loadBankFile("assets/sfx/desktop/Master.strings.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &mainBank);
+    result = system->loadBankFile("assets/sfx/Desktop/Master.strings.bank", FMOD_STUDIO_LOAD_BANK_NORMAL, &mainBank);
     FMOD::Studio::EventDescription *loadedEventDescription = nullptr;
     result = system->getEvent("event:/main", &loadedEventDescription);
     FMOD::Studio::EventInstance *loadedEventInstance = nullptr;
