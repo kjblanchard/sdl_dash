9eb03eb302754e571b6c4305874bac626880608e
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-06-20 12:11:02 -0400

    refactor gametime

--- supergoon_engine/supergoon_engine/primitives/gametime.cpp
+++ supergoon_engine/supergoon_engine/primitives/gametime.cpp
@@ -1,33 +1,29 @@
 #include <supergoon_engine/primitives/gametime.hpp>
 #include <iostream>
 
-Gametime::Gametime()
-{
-    start_time = current_time = SDL_GetTicks64();
-}
-Gametime::~Gametime()
+Gametime::Gametime() : start_time{SDL_GetTicks64()}, current_time{SDL_GetTicks64()}
 {
 }
 
 void Gametime::Tick()
 {
     current_time = SDL_GetTicks64();
-    _previousGameRunningTime = _currentGameRunningTime;
-    _currentGameRunningTime = current_time;
-    _thisTickDeltaTime = _currentGameRunningTime - _previousGameRunningTime;
-    _timeSinceLastUpdate += _thisTickDeltaTime;
+    previous_game_running_time = current_game_running_time;
+    current_game_running_time = current_time;
+    this_tick_delta_time = current_game_running_time - previous_game_running_time;
+    time_since_last_update += this_tick_delta_time;
 }
 
 void Gametime::UpdateClockTimer()
 {
-    _timeSinceLastUpdate -= _msPerFrame;
-    if (_timeSinceLastUpdate < 0)
-        _timeSinceLastUpdate = 0;
+    time_since_last_update -= MillisecondsPerFrame();
+    if (time_since_last_update < 0)
+        time_since_last_update = 0;
 }
 
 ushort Gametime::CheckForSleepTime()
 {
-    auto timeToWait = _msPerFrame - _timeSinceLastUpdate;
+    auto timeToWait = MillisecondsPerFrame() - time_since_last_update;
     if (timeToWait < 0)
         return 0;
     auto wait_time_int = static_cast<ushort>(ceil(timeToWait));
--- supergoon_engine/supergoon_engine/primitives/gametime.hpp
+++ supergoon_engine/supergoon_engine/primitives/gametime.hpp
@@ -3,54 +3,87 @@
 #include <SDL.h>
 #include <supergoon_engine_export.h>
 
+/**
+ * Gametime is passed in gameobjects update call for elapsed time this frame. Pass by ref.
+ *
+ * Besides having elapsed time between frames, gametime is crucial in the world.cpp file for handling sleep time between frames.
+ */
 class SUPERGOON_ENGINE_EXPORT Gametime
 {
+
 private:
+    int game_fps = 60;
     Uint64 start_time;
     Uint64 current_time;
+    double previous_game_running_time = 0.0;
+    double current_game_running_time = 0.0;
+    double this_tick_delta_time = 0.0;
+    double time_since_last_update = 0.0;
 
-    const double _msPerFrame = 1000.00 / 60;
-    double _previousGameRunningTime = 0.0;
-    double _currentGameRunningTime = 0.0;
-    double _thisTickDeltaTime = 0.0;
-    double _timeSinceLastUpdate = 0.0;
+    double MillisecondsPerFrame()
+    {
+        return 1000.00 / game_fps;
+    }
+    /**
+     * Change the current games fps.  Used for delay times and time sent to objects update functions.
+     * @param fps The amount of fps we should set for update time calculations.
+     */
+    void SetGameFps(int fps)
+    {
+        game_fps = fps;
+    }
 
 public:
     Gametime();
-    ~Gametime();
-    double ElapsedTimeInSeconds();
-    //Hello
+    /**
+     * The amount of seconds in this update tick.  Used by gameobjects when calculating frame independant movement in their update.
+     *
+     * @return The amount of seconds this update tick (0.016667 for 60 fps, etc)
+     */
+    double ElapsedTimeInSeconds()
+    {
+        return game_fps / 1000.00;
+    }
+    /**
+     * Gets the amount of milliseconds on this update frame.
+     *
+     * @return The amount of milliseconds this update frame. 16.6667 for 60fps, etc
+     */
     double ElapsedTimeInMilliseconds()
     {
-        return _msPerFrame;
+        return MillisecondsPerFrame();
     }
-    Uint64 TotalGameTimeThisFrame();
+    /**
+     * The total gametime since the game has started in milliseconds.
+     * @return The total amount of milliseconds since the game has started.
+     */
+    Uint64 TotalGameTimeMillisecondsThisFrame();
     /**
      * Checks to see if we should sleep between frames.  Useful to not have 100% cpu when not using vsync.
      * @return The amount of time that we should sleep on this tick
      */
     ushort CheckForSleepTime();
     /**
-     * \brief This function should run all the time to determine if it should run a new frame
+     * This function should run all the time to determine if it should run a new frame
      */
     void Tick();
     /**
-     * \brief This is called when the game performs a update of the game loop, which will subtract from the amount of time needed to update
+     * This is called when the game performs a update of the game loop, which will subtract from the amount of time needed to update
      */
     void UpdateClockTimer();
     /**
-     * \brief Returns if the time since last update is greater than the ms per frame
-     * \return If the game should perform a game loop update
+     * Returns if the time since last update is greater than the ms per frame
+     * @return If the game should perform a game loop update
      */
-    bool ShouldUpdate() const { return _timeSinceLastUpdate >= _msPerFrame; }
+    bool ShouldUpdate() { return time_since_last_update >= MillisecondsPerFrame(); }
     /**
-     * \brief Checks to see if the game is getting a steady frame rate or not
-     * \return Value is true if the Update time since last frame is the ms frame time * 1.5
+     *  Checks to see if the game is getting a steady frame rate or not
+     * @return Value is true if the Update time since last frame is the ms frame time * 1.5
      */
-    bool GameIsLagging() const { return _timeSinceLastUpdate >= (_msPerFrame * 1.5); }
+    bool GameIsLagging() { return time_since_last_update >= (MillisecondsPerFrame() * 1.5); }
     /**
-     * \brief Returns the amount of time since the last update was ran
-     * \return The amount of time since the last update was ran
+     * Returns the amount of time since the last update was ran
+     * @return The amount of time since the last update was ran
      */
-    const double &DeltaTime() const { return _timeSinceLastUpdate; }
+    const double &DeltaTime() const { return time_since_last_update; }
 };
