8a090185ab489e178a93117461e597c1bcd33d8e
Kevin Blanchard <kevin.blanchard@menlosecurity.com>
2022-07-09 08:47:43 -0400

    add some to cmake

--- supergoon_dash/assets/tiled/tilesets/backgrounds.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="backgrounds" tilewidth="64" tileheight="64" tilecount="1" columns="0" objectalignment="topleft">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="64" height="64" source="purple.png"/>
- </tile>
-</tileset>
